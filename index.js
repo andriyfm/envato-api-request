480e74501ba74370415dc1ecc92b7e7c670eff94
andriyfm <andrifmsyh@gmail.com>
2018-11-14 23:12:27 +0700

    add getUserBadges method

--- index.js
+++ index.js
@@ -322,6 +322,25 @@ class EnvatoAPI {
     })
   }
 
+  getUserBadges(params) {
+    return new Promise((resolve, reject) => {
+
+      const username = params.username
+
+      if (!username) {
+        return reject(new Error('params.username is required'))
+      }
+
+      if (typeof(username) != 'string') {
+        return reject(new Error('params.username type error'))
+      }
+
+      return resolve(this.get({
+        url: `/market/user-badges:${username}.json`
+      }))
+    })
+  }
+
   getRandomNewFiles(params) {
     return new Promise((resolve, reject) => {
 
